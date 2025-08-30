e5e90c225c6e23750a55a9b24931ab863247a01a
wkdkavishka <w.k.dumindukavishka@gmail.com>
2025-08-31 04:24:54 +0530

    auto deploy 2025-08-31 04:24:54

--- src/app/metadata.ts
+++
@@ -1,7 +0,0 @@
-import { Metadata } from 'next';
-import siteData from '@/data/site-data';
-
-export const metadata: Metadata = {
-    title: siteData.project.name,
-    description: siteData.project.title,
-};
